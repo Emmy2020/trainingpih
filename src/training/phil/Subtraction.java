12fe9749d2587c8c2ff7e1fe5716e975de8dc0b9
Emmy2020 <eyezu13@gmail.com>
2016-08-02 11:17:18 +0200

    Adding subtraction class

---
+++ src/training/phil/Subtraction.java
@@ -0,0 +1,12 @@
+package training.phil;
+
+public class Subtraction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n1=23,n2=35;
+		System.out.println(n1-n2);
+	}
+
+}
